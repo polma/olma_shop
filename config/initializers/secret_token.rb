7401dc46a913d6d2d5a078588399c3849f592d79
Piotr Olma <perdo.olma@gmail.com>
2012-12-17 21:18:34 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+OlmaShop::Application.config.secret_token = '14c647e58f2f0b2843d2d13f9dccdf7c15d6ad3d33d55ed62190d78dc974e70eab584196872b27418c151330077754622b3bfa294318e23d6dc86cde2ebe489d'
