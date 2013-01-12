4d8478e0c8710be2d07586cd349cab603a23cb1a
Piotr Olma <perdo.olma@gmail.com>
2013-01-12 19:20:11 +0100

    Modify Category and Product models to show their relation

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,6 @@
 class Product < ActiveRecord::Base
   attr_accessible :category_id, :description, :name, :price
+  
+  belongs_to :category
+  validates :price, :numericality => { :only_integer => true }
 end
