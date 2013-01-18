df7ebb701864905502e293949757576ab53bfbfe
Piotr Olma <perdo.olma@gmail.com>
2013-01-18 22:14:49 +0100

    Change smtp settings for heroku

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,14 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 OlmaShop::Application.initialize!
+
+
+config.action_mailer.delivery_method = :smtp
+
+ActionMailer::Base.smtp_settings = {
+  :address              => "smtp.gmail.com",
+  :port                 => 587,
+  :user_name            => 'smsdispatcher.android@gmail.com',
+  :password             => 'dispatch!@#',
+  :authentication       => 'plain',
+  :enable_starttls_auto => true  }
